e281f0e424d19d9b171b4c2ecf4cc2e4220cf026
MiaMcClure <mia.d.mcclure@gmail.com>
2023-11-16 15:29:51 -0800

    updated parks search cards

--- scripts/parks.js
+++ scripts/parks.js
@@ -4,7 +4,7 @@ window.onload = init;
 function init() {
     // add placeholder images to cards
     let image = 'img';
-    let imageValue = "park-placeholder.jpeg";
+    let imageValue = "images/extra/park-placeholder.jpg";
     for (let i = 0; i < nationalParksArray.length; i++) {
         nationalParksArray[i].image = imageValue;
     };
@@ -61,14 +61,33 @@ function init() {
     // create bootstrap card
     function createCard(_park) {
         return `
-        <div class="card" style="width: 18rem;">
-        <img src="${_park.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${_park.LocationName}</h5>
-            <p class="card-text">${_park.State}.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="maincontainer">
+        <div class="thecard">
+            <!-- front -->
+            <div class="thefront" data-overlay-text="${_park.LocationName}"
+                style="background-image: url('${_park.image}');">
+            </div>
+            <!-- back -->
+            <div class="theback">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="back-img" src="${_park.image}" alt="park-image">
+                    </div>
+                    <div class="park-info col-8 mt-3">
+                        <h5 class="card-title">${_park.LocationName}</h5>
+                        <br>
+                        <p>Visit ${_park.City}, ${_park.State} to experience stunningviews, watch wildlife, and learn about the
+                            inspiring history of this park.</p>
+                        <br>
+                        <p>Lorem, ipsum dolor sit amet  elit. Ad, quosreiciendis.</p>
+                        <a href="${_park.Visit}" class="btn btn-primary">Visit Site</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>`;
+
+    </div>
+        `;
     }
 
     // event listener
